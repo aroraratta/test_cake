c7a7946b9d1b651316bdef5eb8d3a7cf98fb563e
EC2 Default User <177751775+aroraratta@users.noreply.github.com>
2025-02-10 10:32:57 +0000

    [Add]管理者ログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
   get "homes/about"
-  
-  devise_for :customers, skip: [:passwords], controllers: {
+
+  devise_for :customers, skip: [:confirmations, :omniauth_callbacks, :passwords, :unlocks], controllers: {
     registrations: "public/registrations",
     sessions: "public/sessions"
   }
-  end
+
+  devise_for :admins, skip: [:confirmations, :omniauth_callbacks, :passwords, :registrations, :unlocks], controllers: {
+    sessions: "admins/sessions"
+  }
+end
