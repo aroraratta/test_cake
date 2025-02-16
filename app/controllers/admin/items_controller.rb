6437318c70a0d618473a876c210d3fd4c1405cd4
EC2 Default User <177751775+aroraratta@users.noreply.github.com>
2025-02-16 08:49:34 +0000

    [Add]商品編集機能実装

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -18,6 +18,11 @@ class Admin::ItemsController < ApplicationController
 
   def update
     @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to admin_item_path(@item)
+    else
+      render :edit
+    end
   end
 
   def index
