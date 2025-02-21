474f0bdb250563988248fe2e8bdbfef5403c3220
EC2 Default User <177751775+aroraratta@users.noreply.github.com>
2025-02-21 12:04:31 +0000

    [Add]top画面のレイアウト調整途中

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,12 @@ class Item < ApplicationRecord
   belongs_to :genre
 
   has_one_attached :item_image
+
+  def get_item_image
+    unless item_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.png')
+      item_image.attach(io: File.open(file_path), filename: 'no_image.png', content_type: 'image/png')
+    end
+    item_image
+  end
 end
