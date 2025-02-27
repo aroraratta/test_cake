b4b2d8962e7a93c69a843bff22cc15ebd59b9e49
EC2 Default User <177751775+aroraratta@users.noreply.github.com>
2025-02-27 13:39:00 +0000

    [Add]顧客編集コントローラー実装

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -4,4 +4,23 @@ class Public::CustomersController < ApplicationController
     @customer = current_customer
   end
 
+  def edit
+    @genres = Genre.only_active
+    @customer = current_customer
+  end
+  
+  def update
+    if @customer.update(customer_params)
+      redirect_to mypage_path
+    else
+      render :edit
+    end
+  end
+  
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :first_name_kana, :last_name_kana, :email, :postal_code, :adress, :telephone_number)
+  end
+
 end
