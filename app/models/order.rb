e2c577eccabe2f5527427d5dbcf168beedfe141f
EC2 Default User <177751775+aroraratta@users.noreply.github.com>
2025-04-23 12:47:18 +0000

    [Add]アソシエーション設定

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ApplicationRecord
+  has_many :order_details
+  has_many :items, through: :order_details
+
   enum payment_method: { credit_card: 0, transfer: 1 }
   
   def finally_address
