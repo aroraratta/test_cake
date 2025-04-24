f532e7c75abee88f5c24f17a0b7bc3ab198473ff
EC2 Default User <177751775+aroraratta@users.noreply.github.com>
2025-04-24 14:50:53 +0000

    [Fix]アソシエーション修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ApplicationRecord
   has_many :order_details
+  belongs_to :customer
   has_many :items, through: :order_details
 
   enum payment_method: { credit_card: 0, transfer: 1 }
