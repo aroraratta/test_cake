ba25d724abbd38323c905b882855d453493ac88f
EC2 Default User <177751775+aroraratta@users.noreply.github.com>
2025-04-25 14:41:47 +0000

    [Fix]注文確定機能修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,7 +5,19 @@ class Order < ApplicationRecord
 
   enum payment_method: { credit_card: 0, transfer: 1 }
   
-  def finally_address
-    '〒' + postal_code + ' ' + destination + ' ' + name
+  def create_order_details(customer)
+    unless order_details.first
+      cart_items = customer.cart_items.includes(:item)
+      cart_items.each do |cart_item|
+        item = cart_item.item
+        OrderDetail.create!(
+          order_id: id,
+          item_id: item.id,
+          price: item.with_tax_price,
+          amount: cart_item.amount
+        )
+      end
+      cart_items.destroy_all
+    end
   end
 end
